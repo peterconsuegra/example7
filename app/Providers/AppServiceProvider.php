c843f22046b79ccb08e088f5ae7365f2aab1057e
Mojave <mojave@Mojaves-MacBook-Pro.local>
2020-10-27 16:02:24 -0500

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+		if (app()->environment() == 'local' || app()->environment() == 'testing') {
+
+		        $this->app->register(\Summerblue\Generator\GeneratorsServiceProvider::class);
+
+		    }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Project::observe(\App\Observers\ProjectObserver::class);
+
+        //
+    }
+}
